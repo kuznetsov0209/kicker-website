21cc4cb83bb5befab3f1cd1de045bb9d46bfd646
Dmitry Zebrov <dmzebrov@gmail.com>
2019-11-26 20:14:16 +0000

    Change max-width and padding-top

--- src/entry/App.styles.js
+++ src/entry/App.styles.js
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 
 export const Main = styled.div`
   width: 100%;
-  max-width: 850px;
+  max-width: 1160px;
   margin: 0 auto;
-  padding-top: 120px;
+  padding-top: 90px;
   padding-left: 16px;
   padding-right: 16px;
   box-sizing: border-box;
